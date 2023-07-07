f79ad8d0c3f1bd35a28d69360370eed58493268b
陈俊丞 <chenjuncheng888@gmail.com>
2023-07-08 00:36:47 +0800

    v0.5.7 beta

--- run_fmu_GPC.py
+++ run_fmu_GPC.py
@@ -1,8 +1,8 @@
 from GPC_universal import *
-from model_fmu_dynamics import *
+from model_fmu_dynamics import model_fmu_dynamics, plant_fmu_dynamics
 
 if __name__ == "__main__":
-    L = 10 * 3600
+    L = 20 * 3600
     Ts = 10 * 60
     # 仿真次数
     n = int(L / Ts)
@@ -32,28 +32,35 @@ if __name__ == "__main__":
     # 控制量限制
     du_limit_list = [1, 50, 50, 50]
     u_limit_list = [[6, 15], [350, 3500], [450, 4200], [30, 300]]
-    # 系统动态模型
+
+    # MMGPC内置系统动态模型
     ans_model = model_fmu_dynamics()
-    model_list = ans_model[1]
+    model_list = ans_model[0]
     Np_list = ans_model[3]
     Nc_list = ans_model[4]
     r_list = ans_model[5]
     q_list = ans_model[6]
-    index = 1
+    # s_list = ans_model[7]
+    # 用于测试的plant_model
+    ans_plant = plant_fmu_dynamics()
+    plant_list = ans_plant[0]
+
     # smgpc
-    smgpc(L, Ts, Np_list[index], Nc_list[index], model_list[index], r_list[index], q_list[index],
-          yr_list, yr_0_list, u_0_list, du_limit_list, u_limit_list, True)
+    model_index = 4
+    smgpc(L, Ts, Np_list[model_index], Nc_list[model_index], model_list[model_index], r_list[model_index],
+          q_list[model_index], yr_list, yr_0_list, u_0_list, du_limit_list, u_limit_list, True)
+
     # # mmgpc
+    # plant_index = 1
     # # s_list: 多模型权值系数的递推计算收敛系数，列表
     # s_list = [1000]
     # # V: 一个非常小的正实数，保证所有子控制器将来可用
     # V = 0.0001
     # # 将初始化的控制器参数数据保存下来的路径
     # file_path_init = './model_data/GPC_data'
-    # plant_list = model_list[index]
     # save_data_init = True
     # plot_set = True
     # model_plot_set = True
-    # mmgpc(L, Ts, Np_list, Nc_list, s_list, V, plant_list, model_list, r_list, q_list, yr_list, yr_0_list,
+    # mmgpc(L, Ts, Np_list, Nc_list, s_list, V, plant_list[plant_index], model_list, r_list, q_list, yr_list, yr_0_list,
     #       u_0_list, du_limit_list, u_limit_list, file_path_init, save_data_init, plot_set, model_plot_set)
 
